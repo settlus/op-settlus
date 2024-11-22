461fc0e28edda04ea303da74eefdfbede749d211
uhyun <96884119+uhyunpark@users.noreply.github.com>
2024-11-22 11:47:43 +0900

    protect tenant manager scheduling

--- contracts/scripts/checker.ts
+++ contracts/scripts/checker.ts
@@ -6,6 +6,8 @@ import { vars } from "hardhat/config"
 async function main() {
   const tm = await hre.ethers.getContractAt('TenantManager', addresses.tenantManagerProxy)
   const tenants = await tm.getTenantAddresses({ gasLimit: 100000000 })
+  const tenantSettlementSchedules = await tm.getTenantSettlementSchedules()
+  console.log('Tenant settlement schedules:', tenantSettlementSchedules)
 
   // Check each tenant's lastSettledIndex and treasury balance
   for (const tenantAddress of tenants) {
