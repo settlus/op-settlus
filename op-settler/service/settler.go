9c7e08aa7610cbe3cee408618b1aa194f5537a7b
Uhyun Park <96884119+uhyunpark@users.noreply.github.com>
2025-01-06 13:50:04 +0900

    feat: update tx check logic (#18)
    * feat: update tx check logic
    
    * remove wait group
    
    * why omitted

--- op-settler/service/settler.go
+++ op-settler/service/settler.go
@@ -3,7 +3,6 @@ package service
 import (
 	"context"
 	_ "embed"
-	"encoding/json"
 	"math/big"
 
 	log "github.com/sirupsen/logrus"
@@ -13,7 +12,6 @@ import (
 	"github.com/ethereum/go-ethereum"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -25,6 +23,12 @@ func Start(ctx context.Context) error {
 		log.Errorf("Failed to connect to the Ethereum client: %v", err)
 		return err
 	}
+
+	signer := NewSigner(ctx)
+
+	txChecker := NewTxChecker(client)
+	txChecker.Start(ctx)
+	defer txChecker.Shutdown()
 
 	headerChannel := make(chan *types.Header)
 	sub, err := client.SubscribeNewHead(ctx, headerChannel)
@@ -43,19 +47,26 @@ func Start(ctx context.Context) error {
 			return err
 		case header := <-headerChannel:
 			log.Printf("New block: %v", header.Number.String())
-			err := callSettleAll(ctx, client)
-			if err != nil {
-				log.Errorf("Failed to call settleAll: %v", err)
-			}
+			go func() {
+				tx, msg, err := callSettleAll(ctx, client, signer)
+				if err != nil {
+					log.Errorf("Failed to call settleAll: %v", err)
+				} else {
+					if tx != nil {
+						txChecker.CheckTransaction(&TxCheckMsg{tx, &msg})
+						log.Debugf("Transaction msg: %+v", msg)
+					}
+				}
+			}()
 		}
 	}
 }
 
-func callSettleAll(ctx context.Context, client *ethclient.Client) error {
+func callSettleAll(ctx context.Context, client *ethclient.Client, signer Signer) (*types.Transaction, ethereum.CallMsg, error) {
 	chainID, err := client.NetworkID(ctx)
 	if err != nil {
 		log.Errorf("Failed to get chain ID: %v", err)
-		return err
+		return nil, ethereum.CallMsg{}, err
 	}
 
 	proxyAddress := common.HexToAddress(GetProxyAddress())
@@ -64,33 +75,31 @@ func callSettleAll(ctx context.Context, client *ethclient.Client) error {
 	needSettlement, err := checkNeedSettlement(ctx, client, tenantManagerABI, proxyAddress)
 	if err != nil {
 		log.Errorf("Failed to get settle required tenants: %v", err)
-		return err
+		return nil, ethereum.CallMsg{}, err
 	}
 
 	if !needSettlement {
 		log.Infof("No tenants to settle")
-		return nil
+		return nil, ethereum.CallMsg{}, nil
 	}
 
 	inputData, err := tenantManagerABI.Pack("settleAll")
 	if err != nil {
 		log.Errorf("Failed to pack input data: %v", err)
-		return err
+		return nil, ethereum.CallMsg{}, err
 	}
-
-	signer := NewSigner(ctx)
 
 	fromAddress := signer.PublicAddress()
 	nonce, err := client.PendingNonceAt(ctx, fromAddress)
 	if err != nil {
 		log.Errorf("Failed to get nonce: %v", err)
-		return err
+		return nil, ethereum.CallMsg{}, err
 	}
 
 	gasPrice, err := client.SuggestGasPrice(ctx)
 	if err != nil {
 		log.Errorf("Failed to get gas price: %v", err)
-		return err
+		return nil, ethereum.CallMsg{}, err
 	}
 
 	msg := ethereum.CallMsg{
@@ -102,7 +111,7 @@ func callSettleAll(ctx context.Context, client *ethclient.Client) error {
 	gasLimit, err := client.EstimateGas(ctx, msg)
 	if err != nil {
 		log.Errorf("Failed to estimate gas: %v", err)
-		return err
+		return nil, ethereum.CallMsg{}, err
 	}
 
 	tx := types.NewTx(&types.LegacyTx{
@@ -120,79 +129,24 @@ func callSettleAll(ctx context.Context, client *ethclient.Client) error {
 	signature, err := signer.Sign(txBytes)
 	if err != nil {
 		log.Errorf("Failed to sign transaction: %v", err)
-		return err
+		return nil, ethereum.CallMsg{}, err
 	}
 
 	signedTx, err := tx.WithSignature(latestSigner, signature)
 	if err != nil {
 		log.Errorf("Failed to return signed signature: %v", err)
-		return err
+		return nil, ethereum.CallMsg{}, err
 	}
 
 	err = client.SendTransaction(ctx, signedTx)
 	if err != nil {
 		log.Errorf("Failed to send transaction: %v", err)
-		return err
+		return nil, ethereum.CallMsg{}, err
 	}
 
 	log.Infof("Transaction sent: %s", signedTx.Hash().Hex())
 
-	txReceipt, err := bind.WaitMined(ctx, client, signedTx)
-	if err != nil {
-		log.Errorf("Failed to wait for transaction mining: %v", err)
-		return err
-	}
-
-	gasUsed := txReceipt.GasUsed
-	totalFeeWei := new(big.Int).Mul(new(big.Int).SetUint64(gasUsed), gasPrice)
-	totalFeeEther := new(big.Float).Quo(new(big.Float).SetInt(totalFeeWei), big.NewFloat(1e18))
-	feeString := totalFeeEther.Text('f', 18)
-
-	if txReceipt.Status == types.ReceiptStatusSuccessful {
-		log.Infof("Transaction successful: %s", signedTx.Hash().Hex())
-		log.Debugf("Gas used: %d, Total fee (ether): %s ETH", gasUsed, feeString)
-	} else {
-		log.Warnf("Transaction failed: %s", signedTx.Hash().Hex())
-		tx, _, err := client.TransactionByHash(ctx, signedTx.Hash())
-		if err != nil {
-			log.Debugf("Failed to get transaction by hash: %v", err)
-			return err
-		}
-
-		msg := ethereum.CallMsg{
-			From: fromAddress,
-			To:   tx.To(),
-			Data: tx.Data(),
-			Gas:  gasLimit * 2,
-		}
-
-		result, err := client.CallContract(ctx, msg, txReceipt.BlockNumber)
-		if err != nil {
-			log.Debugf("Failed to call contract: %v", err)
-			return err
-		}
-
-		if len(result) == 0 {
-			log.Debugf("No revert reason returned")
-			return nil
-		}
-
-		log.Debugf("Raw revert reason result: %s", result)
-
-		var decodedResult map[string]interface{}
-		if err := json.Unmarshal(result, &decodedResult); err != nil {
-			log.Debugf("Failed to unmarshal revert reason: %v", err)
-			return err
-		}
-
-		if revertReason, ok := decodedResult["error"].(string); ok {
-			log.Debugf("Revert reason: %s", revertReason)
-		} else {
-			log.Debugf("Failed to parse revert reason")
-		}
-	}
-
-	return nil
+	return signedTx, msg, nil
 }
 
 func checkNeedSettlement(ctx context.Context, client *ethclient.Client, tenantManagerABI *abi.ABI, contractAddress common.Address) (bool, error) {
