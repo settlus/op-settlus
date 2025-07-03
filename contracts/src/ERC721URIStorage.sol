f791099934981eb8a91199e03bc9045ed27ab13f
uhyun <96884119+uhyunpark@users.noreply.github.com>
2025-07-03 14:49:27 +0900

    update nft contract to receive tokenId

--- contracts/src/ERC721URIStorage.sol
+++ contracts/src/ERC721URIStorage.sol
@@ -6,16 +6,13 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BaseNFT is ERC721URIStorage, Ownable {
-  uint256 private _nextTokenId;
-
   constructor(string memory name, string memory symbol, address initialOwner) ERC721(name, symbol) Ownable(initialOwner) {}
 
-  function safeMint(address to, string memory uri) public onlyOwner returns (uint256) {
-    uint256 tokenId = _nextTokenId;
-    _mint(to, tokenId);
+  function safeMint(address to, uint256 tokenId, string memory uri) public onlyOwner returns (uint256) {
+    //Throws ERC721InvalidSender if tokenId already exists
+    _safeMint(to, tokenId);
     _setTokenURI(tokenId, uri);
 
-    _nextTokenId++;
     return tokenId;
   }
 
